f3e16c2ed90c58a66ebe4135ac6412b5a2f08f2c
SAJITH C <80041370+sajithcs41@users.noreply.github.com>
2021-04-15 17:03:50 +0530

    Add files via upload

---
+++ attandance system 1.py
@@ -0,0 +1,77 @@
+import os
+from datetime import datetime
+
+import face_recognition
+import cv2
+import numpy as np
+
+#url = 'http://192.168.43.1:8080/video'
+path = 'E:/Users/sajit/Downloads/project/'
+images = []  # LIST CONTAINING ALL THE IMAGES
+className = []  # LIST CONTAINING ALL THE CORRESPONDING CLASS Names
+myList = os.listdir(path)
+print("Total Classes Detected:", len(myList))
+for x, cl in enumerate(myList):
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    className.append(os.path.splitext(cl)[0])
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('E:/Users/sajit/Downloads/Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in line:
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M:%S")
+            f.writelines(f'\n{name},{dt_string}')
+
+
+encodeListKnown = findEncodings(images)
+print('Encodings Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+
+        if faceDis[matchIndex] < 0.50:
+            name = className[matchIndex].upper()
+            markAttendance(name)
+        else:
+            name = 'Unknown'
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+    #cv2.destroyAllWindows()
